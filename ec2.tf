15d92a9eaa462940043c147999dd64b7af6d213d
Rajiv <ambanasommillord@yahoo.com>
2021-07-22 23:28:11 -0400

    updated

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,12 @@
+resource "aws_vpc" "jjtech-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "jjtech-vpc"
+  }
+}
+
 resource "aws_instance" "jjtech-test" {
   ami           = "ami-0dc2d3e4c0f9ebd18"
   instance_type = "t2.micro"
